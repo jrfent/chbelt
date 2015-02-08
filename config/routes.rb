509cc13b553eab5c6e43c8ee2d68fb7f210e6d9b
JR Fent <jrfent@gmail.com>
2015-02-08 11:28:52 -0800

    pdf issue solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   resources :subcategories
-
   resources :categories
-
   resources :products
-
   devise_for :users
   get "about" => "pages#about"
   root "products#index"
